9d76d371a477d19228df4deca8c6c751409946c7
あると <mail@twinkfrag.net>
2015-07-08 21:29:33 +0900

    RichTextBoxにバインディングして変更から中身を取得する方法が分からない

---
+++ HyperTextCSharp/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System.Windows.Documents;
+using Reactive.Bindings;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using System.Windows;
+using Livet;
+
+namespace twinkfrag.HyperTextCSharp.ViewModels
+{
+	public class MainWindowViewModel : ViewModel
+	{
+		//public ReactiveProperty<FlowDocument> RichDocuments { get; }
+
+		private FlowDocument _RichDocuments = new FlowDocument();
+
+		public FlowDocument RichDocuments
+		{
+			get { return _RichDocuments; }
+			set
+			{
+				_RichDocuments = value;
+				RaisePropertyChanged();
+			}
+		}
+
+		public ReactiveProperty<BlockCollection> RichBlocks { get; } = new ReactiveProperty<BlockCollection>();
+
+		public ReactiveProperty<string> TextDocument { get; } = new ReactiveProperty<string>();
+
+		public MainWindowViewModel()
+		{
+			//RichDocuments.PropertyChanged += (s, e) => {  };
+
+			//TextDocument = RichDocuments
+			//	.Select(x => x.ToString())
+			//	.ToReactiveProperty();
+			this.PropertyChanged += (sender, e) =>
+			{
+				//if (e.PropertyName == nameof(RichDocuments))
+					MessageBox.Show("changed");
+			};
+		}
+	}
+}
---
+++ HyperTextCSharp/Views/BindableRichTextBox.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+
+namespace twinkfrag.HyperTextCSharp.Views
+{
+	public class BindableRichTextBox : RichTextBox, INotifyPropertyChanged
+	{
+		#region 依存関係プロパティ
+		public static readonly DependencyProperty DocumentProperty =
+			DependencyProperty.Register("Document", typeof(FlowDocument),
+				typeof(BindableRichTextBox), new FrameworkPropertyMetadata(null, OnRichTextItemsChanged));
+
+		private static void OnRichTextItemsChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+		{
+			var control = sender as RichTextBox;
+			if (control != null) control.Document = e.NewValue as FlowDocument;
+		}
+		#endregion
+
+
+		#region 公開プロパティ
+		public new FlowDocument Document
+		{
+			get { return (FlowDocument)GetValue(DocumentProperty); }
+			set
+			{
+				SetValue(DocumentProperty, value);
+				RaisePropertyChanged();
+			}
+		}
+		#endregion
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		protected virtual void RaisePropertyChanged([CallerMemberName] string propertyName = null)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+	}
+}
